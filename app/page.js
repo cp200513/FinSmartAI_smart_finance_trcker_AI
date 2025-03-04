9f9d499ea4fdc84375f051baed0ae12d44aa8213
cp200513 <v.v.chiranjeevprasannaa@gmail.com>
2025-03-05 01:22:37 +0530

    Day 1

--- app/page.js
+++ app/page.js
@@ -3,9 +3,9 @@ import { Calendar } from "@/components/ui/calendar";
 
 export default function Home() {
   return (
-    <>
+    <div className="mt-40">
       <Button variant="destructive">CP</Button>
       <Calendar date={new Date()} />
-    </>
+    </div>
   );
 }
--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b shadow-md">
+      <nav className="container mx-auto p-4 flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src={"/new_logo.png"}
+            alt="CP logo"
+            height={60}
+            width={200}
+            className="h-12 w-auto  object-contain
+            rounded-md shadow-lg"
+          />
+        </Link>
+
+        <SignedOut>
+          <SignInButton forceRedirectUrl="/dashboard">
+            <Button varient="outline">Login</Button>
+          </SignInButton>
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
